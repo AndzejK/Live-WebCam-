73ed05cc046c918598ccf7e988c73c7675b7d9d6
Andzej Krupoves <andrejkrupovec@gmail.com>
2023-04-14 09:29:48 +0300

    take a screenshot each X min

--- capture_screenshot.py
+++ capture_screenshot.py
@@ -1,3 +1,14 @@
 from mss import mss
-with mss() as screen:
-    screen.shot(output='screenshots/scrshot_v2.png')
+from datetime import datetime
+import time
+
+structure_for_date_and_time="%Y-%m-%d_%H:%M:%S"
+
+while True:
+    current_time=datetime.utcnow() 
+    current_time_str=current_time.strftime(structure_for_date_and_time)
+    with mss() as screen:
+        file_name=f'screenshots/scrshot_{current_time_str}.png'
+        screen.shot(output=f'screenshots/scrshot_{current_time_str}.png')
+        print(file_name)
+    time.sleep(60)
