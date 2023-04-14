582b7fd89a068b47fe4a31ce42c2e1a4e2d34302
Andzej Krupoves <andrejkrupovec@gmail.com>
2023-04-14 08:59:29 +0300

    Live Web Cam can be run on the same network

--- intro.py
+++ intro.py
@@ -3,11 +3,12 @@ from flask import Flask,render_template, Response
 video=cv2.VideoCapture(0) # access the cammera 0 or 1
 # generator
 def get_frame():
-    success, frame=video.read()
-    sc,encoded_img=cv2.imencode('.jpg',frame)
-    frame=encoded_img.tobytes()
-    # here we create an image
-    yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n'+frame + b'\r\n')
+    while True:
+        success, frame=video.read()
+        sc,encoded_img=cv2.imencode('.jpg',frame)
+        frame=encoded_img.tobytes()
+        # here we create an image
+        yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n'+frame + b'\r\n')
 
 # Flask instance
 app = Flask(__name__)
@@ -17,7 +18,12 @@ def index():
 
 @app.route("/video")
 def video_feed():
-    return Response(get_frame, mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(get_frame(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+# Run the app locally 
+# if __name__=="__main__":
+#     app.run(debug=True) 
+
+# Run the app on the same network 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0",port=33333) # My IP 192.168.0.103 
